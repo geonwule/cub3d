2d13ab90f3f5cee32028ab0c4adb1d8baf124a6a
Jongwon Choi <jonchoi@c1r9s7.42seoul.kr>
2023-06-28 04:25:55 +0900

    feat: 맵 유효성 검사

---
+++ Cub3d/srcs/parsing/check_map.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_map.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jonchoi <jonchoi@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/28 02:35:03 by jonchoi           #+#    #+#             */
+/*   Updated: 2023/06/28 04:24:18 by jonchoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+static int	count_plyer_and_is_valid(char **map)
+{
+	int	i;
+	int	j;
+	int	cnt;
+
+	cnt = 0;
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (!ft_strchr("NWES01 ", map[i][j]))
+				return (-1);
+			if (ft_strchr("NWES", map[i][j]))		// set players pos
+				cnt++;								// set_pos();
+			j++;
+		}
+		i++;
+	}
+	return (cnt);
+}
+
+static int	check_edge(int i, int j, t_vars *vars)
+{
+	char	**map;
+
+	map = vars->map.arr;
+	if ((i == 0 || i == (vars->map.height - 1) \
+		|| j == 0 || j == (vars->map.width - 1)) \
+		&& (map[i][j] != ' ' && map[i][j] != '1'))
+		return (1);
+	return (0);
+}
+
+static int	check_dir(int i, int j, t_vars *vars)
+{
+	int			nx;
+	int			ny;
+	const int	dx[4] = {0, 1, 0, -1};
+	const int	dy[4] = {1, 0, -1, 0};
+	int			k;
+
+	k = 0;
+	while (k < 4)
+	{
+		nx = i + dx[k];
+		ny = j + dy[k];
+		if (nx < 0 || nx >= vars->map.height || ny < 0 || ny >= vars->map.width)
+		{
+			k++;
+			continue ;
+		}
+		if (vars->map.arr[nx][ny] == ' ' \
+			&& (vars->map.arr[i][j] != '1' && vars->map.arr[i][j] != ' '))
+			return (1);
+		k++;
+	}
+	return (0);
+}
+
+int	check_map(t_vars *vars)
+{
+	char	**map;
+	int		i;
+	int		j;
+
+	map = vars->map.arr;
+	if (count_plyer_and_is_valid(map) != 1)
+	{
+		printf("player is not one or invalid map\n");	// del
+		return (1);
+	}
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (check_edge(i, j, vars))
+			{
+				printf("[%d][%d] is not wall\n", i, j); // del
+				return (1);
+			}
+			if (check_dir(i, j, vars))
+			{
+				printf("[%d][%d] is not wall2\n", i, j); // del
+				return (1);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
