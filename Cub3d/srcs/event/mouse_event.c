d34a16619f50e91c50568a76e6ce08b68f05fd3d
Jongwon Choi <jonchoi@c8r7s1.42seoul.kr>
2023-07-04 04:44:29 +0900

    docs: check norm; mouse_event.c

--- Cub3d/srcs/event/mouse_event.c
+++ Cub3d/srcs/event/mouse_event.c
@@ -6,7 +6,7 @@
 /*   By: geonwule <geonwule@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 19:15:25 by geonwule          #+#    #+#             */
-/*   Updated: 2023/06/27 13:42:03 by geonwule         ###   ########.fr       */
+/*   Updated: 2023/07/04 04:44:00 by jonchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,8 @@ int	handle_mouse_button(int button, int x, int y, void *args)
 	else if (vars->data.npc_talk && vars->data.quest_num == 0 \
 		&& x >= 800 && x <= 850 && y >= 420 && y <= 435)
 		talk_npc_agree(vars);
-	else if (vars->data.npc_talk && ((x >= 870 && x <= 920 && y >= 420 && y <= 435) \
+	else if (vars->data.npc_talk \
+		&& ((x >= 870 && x <= 920 && y >= 420 && y <= 435) \
 		|| (x >= 435 && x <= 510 && y >= 420 && y <= 435)))
 		talk_npc_negative(vars);
 	else if (button == 1 && vars->data.dead_check == 0 && !vars->data.npc_talk)
@@ -50,14 +51,13 @@ int	handle_mouse_button(int button, int x, int y, void *args)
 int	handle_mouse_move(int x, int y, void *args)
 {
 	t_vars	*vars;
-	vars = (t_vars *)args;
 
+	vars = (t_vars *)args;
 	if (vars->data.npc_talk)
 		return (0);
 	vars->data.mouse_old_x = vars->data.mouse_x;
 	vars->data.mouse_old_y = vars->data.mouse_y;
 	vars->data.mouse_x = x;
 	vars->data.mouse_y = y;
-	//printf("in mouse_handel\nx = %d old_x = %d, vars = %p\n", vars->mouse_x, vars->mouse_old_x, vars);
 	return (0);
 }
