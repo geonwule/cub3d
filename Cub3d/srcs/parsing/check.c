c97a663bdab8bf7c54c2d60bc62f259d0316f179
Geonwu Lee <geonwlee22@gmail.com>
2023-07-05 15:18:49 +0900

    docs: jonn split부분 수정중 화이팅

--- Cub3d/srcs/parsing/check.c
+++ Cub3d/srcs/parsing/check.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jonchoi <jonchoi@student.42seoul.k>        +#+  +:+       +#+        */
+/*   By: geonwule <geonwule@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 20:56:22 by jonchoi           #+#    #+#             */
-/*   Updated: 2023/07/03 22:19:27 by jonchoi          ###   ########.fr       */
+/*   Updated: 2023/07/05 15:18:12 by geonwule         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,13 @@ int	check_texture(char *line)
 			|| !ft_strncmp(arr[0], "SO", ft_strlen(arr[0])) \
 			|| !ft_strncmp(arr[0], "WE", ft_strlen(arr[0])) \
 			|| !ft_strncmp(arr[0], "EA", ft_strlen(arr[0])))
-		return (1);
+		{
+			int end = (int)ft_strlen(arr[1]) - 1;
+			if (arr[1][end] != '\n')
+				return (0);
+			return (1);
+		}
+
 	return (0);
 }
 
@@ -44,6 +50,11 @@ int	check_color(char *line)
 	arr = ft_split(line, ' ');
 	if (!ft_strncmp(arr[0], "F", ft_strlen(arr[0]))
 		|| !ft_strncmp(arr[0], "C", ft_strlen(arr[0])))
-		return (1);
+		{
+			int end = (int)ft_strlen(arr[1]) - 1;
+			if (arr[1][end] != '\n')
+				return (0);
+			return (1);
+		}
 	return (0);
 }
