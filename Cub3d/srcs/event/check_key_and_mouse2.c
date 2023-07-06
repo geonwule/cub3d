09af1c3009cdc83d0557b727a41b138a9b527e89
Geonwu Lee <geonwlee22@gmail.com>
2023-07-06 12:28:06 +0900

    fix: check_key_and_mouse2 레이케스팅 벽 뚫림 재수정 완료

--- Cub3d/srcs/event/check_key_and_mouse2.c
+++ Cub3d/srcs/event/check_key_and_mouse2.c
@@ -6,7 +6,7 @@
 /*   By: geonwule <geonwule@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 04:11:57 by jonchoi           #+#    #+#             */
-/*   Updated: 2023/07/06 11:08:04 by geonwule         ###   ########.fr       */
+/*   Updated: 2023/07/06 12:25:30 by geonwule         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	move_forward_backward(t_vars *vars, t_info *info, double move_speed)
 			can_move(vars, (int)(info->pos_x + info->dir_x * move_speed + air), \
 			(int)(info->pos_y)))
 		info->pos_x += info->dir_x * move_speed;
+		air = info->dir_y * move_speed * 0.2;
 	if (can_move(vars, (int)(info->pos_x), \
 			(int)(info->pos_y + info->dir_y * move_speed)) \
 			&& can_move(vars, (int)(info->pos_x), \
@@ -33,12 +34,13 @@ void	move_left_right(t_vars *vars, t_info *info, double move_speed)
 {
 	double	air;
 
-	air = info->dir_x * move_speed * 0.2;
+	air = info->dir_y * move_speed * 0.2;
 	if (can_move(vars, (int)(info->pos_x - info->dir_y * move_speed), \
 			(int)(info->pos_y)) && \
 			can_move(vars, (int)(info->pos_x - info->dir_y * move_speed - air), \
 			(int)(info->pos_y)))
 		info->pos_x -= info->dir_y * move_speed;
+	air = info->dir_x * move_speed * 0.2;
 	if (can_move(vars, (int)(info->pos_x), \
 			(int)(info->pos_y + info->dir_x * move_speed)) \
 			&& can_move(vars, (int)(info->pos_x), \
