5e3cd057a70181ec996bf05170be31d99b99b928
Geonwu Lee <geonwlee22@gmail.com>
2023-07-06 11:09:06 +0900

    fix: check_key_and_mouse2 벽 뚫림 현상 수리 완료 /  air 변수 이용

--- Cub3d/srcs/event/check_key_and_mouse2.c
+++ Cub3d/srcs/event/check_key_and_mouse2.c
@@ -6,7 +6,7 @@
 /*   By: geonwule <geonwule@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 04:11:57 by jonchoi           #+#    #+#             */
-/*   Updated: 2023/07/05 20:47:11 by geonwule         ###   ########.fr       */
+/*   Updated: 2023/07/06 11:08:04 by geonwule         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ void	move_forward_backward(t_vars *vars, t_info *info, double move_speed)
 {
 	double	air;
 
-	air = -0.001;
-	if (move_speed < 0.0)
-		air *= -1;
-	if (can_move(vars, (int)(info->pos_x + info->dir_x * move_speed + air), \
-			(int)(info->pos_y + air)))
+	air = info->dir_x * move_speed * 0.2;
+	if (can_move(vars, (int)(info->pos_x + info->dir_x * move_speed), \
+			(int)(info->pos_y)) && \
+			can_move(vars, (int)(info->pos_x + info->dir_x * move_speed + air), \
+			(int)(info->pos_y)))
 		info->pos_x += info->dir_x * move_speed;
-	if (can_move(vars, (int)(info->pos_x + air), \
+	if (can_move(vars, (int)(info->pos_x), \
+			(int)(info->pos_y + info->dir_y * move_speed)) \
+			&& can_move(vars, (int)(info->pos_x), \
 			(int)(info->pos_y + info->dir_y * move_speed + air)))
 		info->pos_y += info->dir_y * move_speed;
 }
@@ -31,13 +33,15 @@ void	move_left_right(t_vars *vars, t_info *info, double move_speed)
 {
 	double	air;
 
-	air = -0.001;
-	if (move_speed < 0.0)
-		air *= -1;
-	if (can_move(vars, (int)(info->pos_x - info->dir_y * move_speed + air), \
-			(int)(info->pos_y + air)))
+	air = info->dir_x * move_speed * 0.2;
+	if (can_move(vars, (int)(info->pos_x - info->dir_y * move_speed), \
+			(int)(info->pos_y)) && \
+			can_move(vars, (int)(info->pos_x - info->dir_y * move_speed - air), \
+			(int)(info->pos_y)))
 		info->pos_x -= info->dir_y * move_speed;
-	if (can_move(vars, (int)(info->pos_x + air), \
+	if (can_move(vars, (int)(info->pos_x), \
+			(int)(info->pos_y + info->dir_x * move_speed)) \
+			&& can_move(vars, (int)(info->pos_x), \
 			(int)(info->pos_y + info->dir_x * move_speed + air)))
 		info->pos_y += info->dir_x * move_speed;
 }
